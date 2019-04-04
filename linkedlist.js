e8639edc066b7593e2510d3469ba1e1d5878f4c9
Robin Khiv <robinkhiv@gmail.com>
2019-04-04 09:39:56 -0700

    display and checking for size in linklist working

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 class _Node {
   constructor(value, next){
     this.value = value;
@@ -16,7 +17,7 @@ class LinkedList {
       this.insertFirst(item);
     else{
       let tempNode = this.head;
-      while(this.head.next !== null)
+      while(tempNode.next !== null)
         tempNode = tempNode.next;
       tempNode.next = new _Node(item, null);
     }
@@ -24,16 +25,150 @@ class LinkedList {
   remove (item) {
     if(!this.head)
       return null;
-    if(this.head.value === value)
+    if(this.head.value === item){
+      this.head = this.head.next;
+      return;
+    } 
+    let currNode = this.head;
+    let previousNode = this.head;
+    while ((currNode !== null) && (currNode.value !== item)) {
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
   }
   find (item) {
-
+    let currNode = this.head;
+    if (!this.head) {
+      return null;
+    }
+    while (currNode.value !== item) {
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        currNode = currNode.next;
+      }
+    }
+    return currNode;
   }
+  insertBefore (item, key){
+    if(this.head === null){
+      console.log('Key does not exist');
+      return null;
+    }
+    let currNode = this.head;
+    let prevNode = this.head;
+    while(currNode.value !== key && currNode !== null){
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+    if(currNode === null){
+      console.log('key not found');
+      return;
+    }
+    prevNode.next = new _Node(item, currNode);
+  }
+  insertAfter (item, key){
+    if(this.head === null){
+      console.log('Key does not exist');
+      return null;
+    }
+    let currNode = this.head;
+    while(currNode.value !== key && currNode !== null){
+      currNode = currNode.next;
+    }
+    if(currNode === null){
+      console.log('key not found');
+      return;
+    }
+    let tempNode = currNode.next;
+    currNode.next = new _Node(item, tempNode);
+  }
+  insertAt(item, position){
+    if(this.head === null){
+      if(position === 1){
+        this.insertFirst(item);
+        return;
+      }
+      else{
+        console.log('position does\'nt exist');
+        return null;
+      }
+    }
+    let currentPosition = 1;
+    let currNode = this.head;
+    let prevNode = this.head;
+    while(currentPosition !== position && currNode !== null){
+      prevNode = currNode;
+      currNode = currNode.next;
+      currentPosition++;
+    }
+    if(currNode === null){
+      console.log('Position not found');
+      return;
+    }
+    let tempNode = currNode;
+    prevNode.next = new _Node(item, tempNode);
+  }
+}
+
+function display (list){
+  let currNode = list.head;
+  if(currNode === null){
+    console.log('List is empty');
+    return;
+  } 
+  return displayHelper(currNode, []);
+}
+
+function displayHelper(node, arr){
+  if(node === null){
+    return []; 
+  }else{
+    return [node.value, ...displayHelper(node.next)];
+  }
+
+}
+function size (list){
+  let currNode = list.head;
+  if(currNode === null){
+    return 'list is empty';
+  }
+  return displayHelper(currNode).length;
+}
+function isEmpty(){
+
+}
+function findPrevious(){
+
+}
+
+function findLast(){
 
 }
 
 function main(){
-  
+  const SLL = new LinkedList();
+  SLL.insertLast('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');
+  SLL.insertLast('Tauhida');
+  console.log(SLL.remove('squirrel'));
+  SLL.insertBefore('Athena', 'Boomer');
+  SLL.insertAfter('Hotdog', 'Helo');
+  SLL.insertAt('Kat', 3);
+  SLL.remove('Tauhida');
+ console.log('display', display(SLL));
+ console.log('size', size(SLL));
 }
+
+
 
 main();
